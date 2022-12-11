6962d88c437b0d48c577f570e1000b5472f3385d
Karli Vare <karli.vare@uconn.edu>
2022-12-11 16:47:58 -0500

    trying to figure out carousel discrepancy between mobile and desktop. got rid of resize animation stopper

--- js/home.js
+++ js/home.js
@@ -1,12 +1,12 @@
 //stops animation while resizing screen
-let resizeTimer;
-window.addEventListener("resize", () => {
-  document.body.classList.add("resize-animation-stopper");
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    document.body.classList.remove("resize-animation-stopper");
-  }, 400);
-});
+// let resizeTimer;
+// window.addEventListener("resize", () => {
+//   document.body.classList.add("resize-animation-stopper");
+//   clearTimeout(resizeTimer);
+//   resizeTimer = setTimeout(() => {
+//     document.body.classList.remove("resize-animation-stopper");
+//   }, 400);
+// });
 
 const navSlide = () =>{ /*creating anonymous function*/
   
